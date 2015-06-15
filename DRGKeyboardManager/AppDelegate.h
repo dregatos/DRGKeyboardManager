c1c9df0d25b7338b50d73e2d927654827db48bcd
David Regatos <david.regatos@gmail.com>
2015-06-15 16:25:09 +0200

    Initial Commit

---
+++ DRGKeyboardManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DRGKeyboardManager
+//
+//  Created by David Regatos on 15/06/15.
+//  Copyright (c) 2015 DRG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DRGKeyboardManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DRGKeyboardManager
+//
+//  Created by David Regatos on 15/06/15.
+//  Copyright (c) 2015 DRG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
